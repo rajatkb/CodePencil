ce554453439956add8d2efe332cdfd69f3b12c41
Meghali Nandi <pisamifa@gmail.com>
2017-03-30 22:21:39 +0530

    Update navbar.js

--- CodePencil Project -v1.0.3 Material Layout/scripts/navbar.js
+++ CodePencil Project -v1.0.3 Material Layout/scripts/navbar.js
@@ -1,10 +1,11 @@
-// JS for the navigation bar 
+//This is the javascript file to bes used for main page functionality 
+
 $(document).ready(function(){
     $("#btn").click(function(){
     
-        $('#box, #overlay-back').fadeIn(500);
+        $('#box, #overlay-back').css("display")=="none"? $("#overlay-back").fadeIn(500):$("#overlay-back").fadeOut(500);
         $("#box").toggleClass("scrollbox");
-        
+    
         
     });
 });
